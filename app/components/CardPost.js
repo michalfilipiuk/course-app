fa903468d1f4ba691e4708b9c5be9d738c606af1
michalfilipiuk <mikef1808@gmail.com>
2025-01-27 14:51:00 +0100

    card post fix

--- app/components/CardPost.js
+++ app/components/CardPost.js
@@ -7,7 +7,7 @@ const CardPost = ({post}) => {
                 <h2 className="font-bold text-lg mb-1">{post.title}</h2>
                 <p className="opacity-80 leading-relaxed max-h-32 overflow-scroll">{post.description}</p>
             </div>
-            <ButtonVote count={post.votesCounter} hasVoted={post.hasVoted} />
+            <ButtonVote count={post.votesCounter} postId={post._id} hasVoted={post.hasVoted} />
         </li>
     )
 }
